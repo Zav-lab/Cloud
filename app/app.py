02da678b6ee4b95222bffbb948a219b5fb8a476b
Andrew <Andrei.Bubnov@urfu.me>
2022-06-05 22:41:55 +0500

    up

--- app/app.py
+++ app/app.py
@@ -8,7 +8,7 @@ from db import conn
 app = Flask(__name__)
 
 app.config.from_pyfile("config.py")
-app.config['JSON_AS_ASCII'] = False # для кириллицы в конфиге
+app.config['JSON_AS_ASCII'] = False
 conn.init_app(app)
 
 app.register_blueprint(bp)
@@ -16,4 +16,4 @@ app.register_blueprint(routes.bp)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")  # machine IP
